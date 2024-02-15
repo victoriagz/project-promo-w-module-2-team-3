b9d47e871d9b923304be321483d96e6c639f9e94
victoriagz <victoriagabrielagonzalez123@hotmail.com>
2024-02-15 14:25:45 +0100

    Funciones JS

--- src/main.js
+++ src/main.js
@@ -1,32 +1,80 @@
 'use strict';
 
+
+// Seleccionar los botones de flecha y contenedores
 const arrowUpDesign = document.querySelector('.js-arrow-up-design');
 const arrowUpForm = document.querySelector('.js-arrow-up-form');
 const arrowUpShare = document.querySelector('.js-arrow-up-share');
-
+const arrowUpShareAfter = document.querySelector('.js-arrow-up-share-after');
 const designContainer = document.querySelector('.js-color-container');
 const formContainer = document.querySelector('.js-form');
 const shareContainer = document.querySelector('.js-share');
+const shareAfterContainer = document.querySelector('.js-share-after');
 
-function showArrowUp(patata) {
-    patata.classList.remove('collapsed');
+
+// Función genérica para mostrar u ocultar un contenedor
+function toggleContainer(container) {
+    container.classList.toggle('collapsed');
 }
 
-function hideArrowUp(patata) {
-    patata.classList.add('collapsed');
+// Manejador de eventos para los botones de flecha
+function handleArrowUp(container) {
+    toggleContainer(container);
 }
 
-function handleArrowUp (event) {
-    event.preventDefault();
-    if (patata.classList.contains('collapsed')) {
-        showArrowUp();
-    } else {
-        hideArrowUp();
-    }
-}
+// Asociar los botones de flecha con sus respectivos contenedores
+arrowUpDesign.addEventListener('click', () => handleArrowUp(designContainer));
+arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer));
+arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer));
+arrowUpShareAfter.addEventListener('click', () => handleArrowUp(shareAfterContainer));
 
-arrowUp.addEventListener('click', handleArrowUp(designContainer));
-arrowUp.addEventListener('click', handleArrowUp(formContainer));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arrowUpDesign = document.querySelector('.js-arrow-up-design');
+// const arrowUpForm = document.querySelector('.js-arrow-up-form');
+// const arrowUpShare = document.querySelector('.js-arrow-up-share');
+
+// const designContainer = document.querySelector('.js-color-container');
+// const formContainer = document.querySelector('.js-form');
+// const shareContainer = document.querySelector('.js-share');
+
+// function showArrowUp(patata) {
+//     patata.classList.remove('collapsed');
+// }
+
+// function hideArrowUp(patata) {
+//     patata.classList.add('collapsed');
+// }
+
+// function handleArrowUp (event) {
+//     event.preventDefault();
+//     if (patata.classList.contains('collapsed')) {
+//         showArrowUp();
+//     } else {
+//         hideArrowUp();
+//     }
+// }
+
+// arrowUp.addEventListener('click', handleArrowUp(designContainer));
+// arrowUp.addEventListener('click', handleArrowUp(formContainer));
 
 
 // function showArrowUpForm() {
