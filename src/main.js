bd48dfe953646aba19af7000326aabb088996506
BiancaMesa <bianca.koraa@gmail.com>
2024-02-19 14:07:48 +0100

    fillout javascript part 1

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ const arrowUpShare = document.querySelector('.js-arrow-up-share');
 const designContainer = document.querySelector('.js-color-container');
 const formContainer = document.querySelector('.js-form');
 const shareContainer = document.querySelector('.js-share');
+const titlePreview = document.querySelector('.js_preview_title'); //variable del preview del nombre 
+const inputName = document.querySelector('.js_name'); //variable nombre del formulario
+const jobPreview = document.querySelector('.js_preview_job');
+const inputJob = document.querySelector('.js_job_position');
+
+
 
 function closeOtherContainers(currentContainer) {
   if (currentContainer !== designContainer) {
@@ -34,6 +40,13 @@ arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer, arrowUp
 arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer, arrowUpShare));
 
 
+// Rellenar automáticamente cardpreview al escribir en el formulario 
+inputName.addEventListener('input', (event) => {
+  titlePreview.innerHTML = event.target.value;
+});
+inputJob.addEventListener('input', (event) => {
+  jobPreview.innerHTML = event.target.value;
+});
 
 // BUTTON CREATE
 const handleClickButtonCreate = (event) => {
