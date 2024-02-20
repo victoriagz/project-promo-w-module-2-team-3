eac566c2657e82fcf3a0d7aa9c245d55d25a34df
Lejla <lejladzanko@gmail.com>
2024-02-20 17:21:56 +0100

    update href attibutes and add required for social links inputs

--- src/js/button-create-reset.js
+++ src/js/button-create-reset.js
@@ -48,3 +48,34 @@ resetForm();
 
 buttonReset.addEventListener('click', handleResetClick);
 
+function updateContactLinks() {
+  // Email
+  const emailInput = document.querySelector('.js_email'); 
+  const emailLink = document.getElementById('social__email__id');
+  emailInput.addEventListener('input', function() {
+    emailLink.href = 'mailto:' + emailInput.value; 
+  });
+
+  // Phone
+  const phoneInput = document.querySelector('.js_phone'); 
+  const phoneLink = document.getElementById('social__mobile__id');
+  phoneInput.addEventListener('input', function() {
+    phoneLink.href = 'tel:' + phoneInput.value; 
+  });
+
+  // LinkedIn
+  const linkedInInput = document.querySelector('.js_linkedin'); 
+  const linkedInLink = document.getElementById('social__linkedin__id');
+  linkedInInput.addEventListener('input', function() {
+    linkedInLink.href = linkedInInput.value; 
+  });
+
+  // GitHub
+  const gitHubInput = document.querySelector('.js_github'); 
+  const gitHubLink = document.getElementById('social__github__id');
+  gitHubInput.addEventListener('input', function() {
+    gitHubLink.href = gitHubInput.value; 
+  });
+}
+
+document.addEventListener('DOMContentLoaded', updateContactLinks);
