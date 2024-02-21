eed3ec067def16fe5704039ff9c7be0563d404c0
BiancaMesa <bianca.koraa@gmail.com>
2024-02-21 23:05:34 +0100

    closing drop-down along with create card when clicking arrow

--- src/js/arrows.js
+++ src/js/arrows.js
@@ -5,6 +5,8 @@ const arrowUpShare = document.querySelector('.js-arrow-up-share');
 const designContainer = document.querySelector('.js-color-container');
 const formContainer = document.querySelector('.js-form');
 const shareContainer = document.querySelector('.js-share');
+const dropDownSection = document.querySelector('.js_drop_down');
+const dropDownSectionCollapsed = dropDownSection.classList.add('collapsed');
 
 
 function closeOtherContainers(currentContainer) {
@@ -18,16 +20,30 @@ function closeOtherContainers(currentContainer) {
       shareContainer.classList.add('collapsed');
     }
   }
+
   
   function toggleContainer(container) {
     container.classList.toggle('collapsed');
   }
+
+  function closeShareContainer (){
+    if (shareContainer.classList.contains('collapsed')){
+      if(dropDownSection !== dropDownSectionCollapsed){
+        dropDownSection.classList.add('collapsed');
+      }
+    }
+  }
+
   function handleArrowUp(container, arrow) {
     closeOtherContainers(container);
     toggleContainer(container);
+    closeShareContainer(); 
     arrow.classList.toggle('rotate-down'); // Agregamos o quitamos la clase rotate-down
   }
+
+ 
   arrowUpDesign.addEventListener('click', () => handleArrowUp(designContainer, arrowUpDesign));
   arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer, arrowUpForm));
   arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer, arrowUpShare));
+  
   
