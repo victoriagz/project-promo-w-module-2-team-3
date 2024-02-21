19add2f49bc930a16e3cae8fe8bb50cd4bc72c8b
Lejla <lejladzanko@gmail.com>
2024-02-21 13:40:13 +0100

    linkedin and github links modified

--- src/js/button-create-reset.js
+++ src/js/button-create-reset.js
@@ -63,18 +63,25 @@ function updateContactLinks() {
     phoneLink.href = 'tel:' + phoneInput.value; 
   });
 
-  // LinkedIn
-  const linkedInInput = document.querySelector('.js_linkedin'); 
-  const linkedInLink = document.getElementById('social__linkedin__id');
-  linkedInInput.addEventListener('input', function() {
-    linkedInLink.href = linkedInInput.value; 
-  });
+ // LinkedIn
+const linkedInInput = document.querySelector('.js_linkedin'); 
+const linkedInLink = document.getElementById('social__linkedin__id');
+
+linkedInInput.addEventListener('input', function() {
+  // URL base de LinkedIn
+  const baseUrl = 'https://www.linkedin.com/in/';
+  
+  // Concatena la URL base con el nombre de usuario ingresado
+  linkedInLink.href = baseUrl + linkedInInput.value; 
+});
 
   // GitHub
   const gitHubInput = document.querySelector('.js_github'); 
   const gitHubLink = document.getElementById('social__github__id');
   gitHubInput.addEventListener('input', function() {
-    gitHubLink.href = gitHubInput.value; 
+    const baseUrl = 'https://www.github.com/';
+  gitHubLink.href = baseUrl + gitHubInput.value; 
+  
   });
 }
 
