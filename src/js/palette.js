3f69ed8eeeab549b33321af77498e7fdb941a28a
Noelia <abascalcabreran@gmail.com>
2024-02-21 18:23:46 +0100

    change palette working

--- src/js/palette.js
+++ src/js/palette.js
@@ -1,66 +1,42 @@
-// //crear un objeto con cada paleta de colores:
-// const colors = [
-//   {
-//     color1: $color - secondary - dark,
-//     color2: $color - secondary - medium,
-//     color3: $color - secondary - light,
-//   },
-//   {
-//     color1: $color - secondary - light,
-//     color2: $color - rusty - red,
-//     color3: $color - tomato,
-//   },
-//   {
-//     color1: $color - slate,
-//     color2: $color - faded - orange,
-//     color3: $color - light - grey - blue,
-//   },
-// ];
+// Creadas las paletas de colores de nuevo, la función no cogía bien las variables scss
+const palette1 = {
+  color1: '#114e4e',
+  color2: '#438792',
+  color3: '#a2deaf',
+};
+const palette2 = {
+  color1: '#4B0101',
+  color2: '#8B3A3A',
+  color3: '#FF6347',
+};
+const palette3 = {
+  color1: '#708090',
+  color2: '#fe994a',
+  color3: '#B7C9E2',
+};
 
-// //crear una función genérica para todos los checkbutton pasandole luego argumentos distintos
-// function handleChangeColor(palette) {
-//   titlePreview.style.color = palette[i].color1;
-//   card_texts.style.border = palette[i].color2;
-//   social_card.style.color = palette[i].color3;
-// }
-// //crear las variables de los radio button sacándolas del HTML
-// const radiobutton1 = document.querySelector('.js-radio1');
-// const radiobutton2 = document.querySelector('.js-radio2');
-// const radiobutton3 = document.querySelector('.js-radio3');
+// Obtenemos del HTML las variables de los radiobuttons:
+const radiobutton1 = document.querySelector('.js-radio1');
+const radiobutton2 = document.querySelector('.js-radio2');
+const radiobutton3 = document.querySelector('.js-radio3');
 
-// //crear un evento para cada radio button
-// radiobutton1.addEventListener('click', handleChangeColor(colors[0]));
-// radiobutton2.addEventListener('click', handleChangeColor(colors[1]));
-// radiobutton3.addEventListener('click', handleChangeColor(colors[2]));
+// Obtenemos del HTML las variables de las partes de la tarjeta para pintarlas:
+const titleCard = document.querySelector('.js_preview_title');
+const borderCard = document.querySelector('.js_border_card');
+const circleCard = document.querySelectorAll('.js_circle_card');
 
-// const palette1 = {
-//   color1: '$color - secondary - dark',
-//   color2: '$color - secondary - medium',
-//   color3: '$color - secondary - light',
-// };
-// const palette2 = {
-//   color1: '$color - secondary - light',
-//   color2: '$color - rusty - red',
-//   color3: '$color - tomato',
-// };
-// const palette3 = {
-//   color1: '$color - slate',
-//   color2: '$color - faded - orange',
-//   color3: '$color - light - grey - blue',
-// };
-// //crear una función genérica para todos los checkbutton pasandole luego argumentos distintos
-// function handleChangeColor(palette) {
-//   titlePreview.style.color = palette.color1;
-//   card_texts.style.borderColor = palette.color2;
-//   social_card.style.color = palette.color3;
-// }
-// //crear las variables de los radio button sacándolas del HTML
-// const radiobutton1 = document.querySelector('.js-radio1');
-// const radiobutton2 = document.querySelector('.js-radio2');
-// const radiobutton3 = document.querySelector('.js-radio3');
+// Creamos una función genérica para todos los radiobuttons pasando luego argumentos distintos.
+// Aplicamos un bucle for en circleCard, porque con querySelectorAll hemos creado un array
+// con los 4 circulos de las redes sociales.
+function handleChangeColor(palette) {
+  titleCard.style.color = palette.color1;
+  borderCard.style.borderColor = palette.color2;
+  for (let i = 0; i < circleCard.length; i++) {
+    circleCard[i].style.borderColor = palette.color3;
+  }
+}
 
-//crear un evento para cada radio button
-// radiobutton1.addEventListener('click', handleChangeColor(palette1));
-// radiobutton2.addEventListener('click', handleChangeColor(palette2));
-// radiobutton3.addEventListener('click', handleChangeColor(palette3));
-// q;
+//Creamos un evento por cada radiobutton, para que aplique una paleta determinada:
+radiobutton1.addEventListener('click', () => handleChangeColor(palette1));
+radiobutton2.addEventListener('click', () => handleChangeColor(palette2));
+radiobutton3.addEventListener('click', () => handleChangeColor(palette3));
