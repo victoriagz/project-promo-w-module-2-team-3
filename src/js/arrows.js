e281b352f73dcd752f39849dcfdbd7c50d129d84
BiancaMesa <bianca.koraa@gmail.com>
2024-02-25 22:44:46 +0100

    loop

--- src/js/arrows.js
+++ src/js/arrows.js
@@ -17,6 +17,7 @@ const inputJob = document.querySelector('.js_job');
 const inputEmail = document.querySelector('.js_email');
 const inputPhone = document.querySelector('.js_phone');
 const requiredMessage = document.querySelector('.js-field-required-message');
+const requiredFields = document.querySelectorAll('required'); //variable que recoge todos los elementos con la clase required
 
 //Form fields values 
 const inputNameValue = inputName.value; 
@@ -24,15 +25,54 @@ const inputJobValue = inputJob.value;
 const inputEmailValue = inputEmail.value; 
 const inputPhoneValue = inputPhone.value; 
 
-// Rellenar automáticamente cardpreview al escribir en el formulario
-inputName.addEventListener('input', (event) => {
-    titlePreview.innerHTML = event.target.value;
-  });
-  inputJob.addEventListener('input', (event) => {
-    jobPreview.innerHTML = event.target.value;
-  });
 
-function closeOtherContainers(currentContainer) {
+  // function handleArrowUpShare () {
+  //   requiredMessage.innerHTML = ''; 
+  //   console.log('0'); 
+  //   //let emptyField = inputNameValue == '' || inputJobValue == '' || inputEmailValue == '' || inputPhoneValue == '';
+  //   let filledOutForm = inputNameValue && inputJobValue && inputEmailValue && inputPhoneValue;
+  
+  //   if (!inputEmail.value.includes('@')) {    
+  //     console.log('2'); 
+  //     requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
+  //     //cambiar color casilla a rojo
+  //   } else if (typeof inputPhoneValue !== 'number') { //= else if (isNaN(inputPhoneValue)) {
+  //     console.log('3'); 
+  //     requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
+  //     //cambiar color casilla a rojo
+  //   } else if (!filledOutForm) {
+  //     console.log('1'); 
+  //     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
+  //     //cambiar color casilla a rojo
+  //   } else {
+  //     handleArrowUp(shareContainer, arrowUpShare);  
+  //   }
+  // }
+
+  //Función manejadora del evento flecha de comparte 
+  function handleArrowUpShare () {
+    console.log('0');
+    requiredMessage.innerHTML = ''; 
+    
+    //Bucle 
+    for (const field of requiredFields) {
+      console.log('1'); 
+
+      if (field.value === '') {
+        console.log('2');
+        field.value.border = '1px solid red;'
+        requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
+
+      } else {
+        console.log('3');
+        field.value.border = ''; 
+        handleArrowUp(shareContainer, arrowUpShare);  
+      }
+    }
+  }
+
+  
+  function closeOtherContainers(currentContainer) {
     if (currentContainer !== designContainer) {
       designContainer.classList.add('collapsed');
     }
@@ -56,68 +96,18 @@ function closeOtherContainers(currentContainer) {
       }
     }
   }
-
  
 
-  function handleArrowUp(container, arrow) {
-    closeOtherContainers(container);
-    toggleContainer(container);
-    closeShareContainer(); 
-    arrow.classList.toggle('rotate-down'); // Agregamos o quitamos la clase rotate-down
-  }
-
-
-  // function handleArrowUpShare () {
-  //   requiredMessage.innerHTML = ''; 
-  //   console.log('entra'); 
-
-  //   if (inputNameValue && inputJobValue && inputEmailValue && inputPhoneValue){
-  //     console.log('hey'); 
-  //     requiredMessage.innerHTML = 'Campos rellenos'; 
-
-  //   } else if (!inputEmailValue.includes('@')){
-  //     console.log('works'); 
-  //     requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
-  //     //cambiar color casilla a rojo
-  //   } else if (inputPhoneValue = NaN){
-  //     console.log('mermaid'); 
-  //     requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
-  //     //cambiar color casilla a rojo
-  //   } else {
-  //     console.log('dog'); 
-  //     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
-  //     //cambiar color casilla a rojo
-  //   }
-  // }
-
-function handleArrowUpShare () {
-  requiredMessage.innerHTML = ''; 
-  console.log('0'); 
-  const emptyField = inputNameValue == '' || inputJobValue == '' || inputEmailValue == '' || inputPhoneValue == '';
-  const filledOutForm = inputNameValue && inputJobValue && inputEmailValue && inputPhoneValue;
-
-  if (!inputEmail.value.includes('@')) {    
-    console.log('2'); 
-    requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
-    //cambiar color casilla a rojo
-  } else if (isNaN(inputPhoneValue)) {
-    console.log('3'); 
-    requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
-    //cambiar color casilla a rojo
-  } else if (emptyField) {
-    console.log('1'); 
-    //requiredMessage.classList.remove('collapsed'); 
-    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
-    //cambiar color casilla a rojo
-  } else if (filledOutForm) {
-    console.log('4'); 
-    handleArrowUp(); 
-  }
+function handleArrowUp(container, arrow) {
+  closeOtherContainers(container);
+  toggleContainer(container); 
+  closeShareContainer(); 
+  arrow.classList.toggle('rotate-down'); // Agregamos o quitamos la clase rotate-down
 }
 
  
   arrowUpDesign.addEventListener('click', () => handleArrowUp(designContainer, arrowUpDesign));
   arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer, arrowUpForm));
-  arrowUpShare.addEventListener('click', (handleArrowUpShare)); 
+  arrowUpShare.addEventListener('click', () => handleArrowUpShare());
   //arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer, arrowUpShare));
   
