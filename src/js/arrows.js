e46a2c6b06d4550e71a9fc245360bc5c73524ebb
BiancaMesa <bianca.koraa@gmail.com>
2024-02-25 20:45:21 +0100

    required field form - part 1

--- src/js/arrows.js
+++ src/js/arrows.js
@@ -1,13 +1,38 @@
 
+//Arrows
 const arrowUpDesign = document.querySelector('.js-arrow-up-design');
 const arrowUpForm = document.querySelector('.js-arrow-up-form');
 const arrowUpShare = document.querySelector('.js-arrow-up-share');
+
+//Containers
 const designContainer = document.querySelector('.js-color-container');
 const formContainer = document.querySelector('.js-form');
 const shareContainer = document.querySelector('.js-share');
 const dropDownSection = document.querySelector('.js_drop_down');
 const dropDownSectionCollapsed = dropDownSection.classList.add('collapsed');
 
+//Form fields
+const inputName = document.querySelector('.js-name');
+const inputJob = document.querySelector('.js-job');
+const inputEmail = document.querySelector('.js-email');
+const inputPhone = document.querySelector('.js-phone');
+const requiredMessage = document.querySelector('.js-field-required-message');
+const titlePreview = document.querySelector('.js_preview_title'); //variable del preview del nombre
+const jobPreview = document.querySelector('.js_preview_job');
+
+//Form fields values 
+// const inputNameValue = inputName.value; 
+// const inputJobValue = inputJob.value; 
+// const inputEmailValue = inputEmail.value; 
+// const inputPhoneValue = inputPhone.value; 
+
+// Rellenar automáticamente cardpreview al escribir en el formulario
+inputName.addEventListener('input', (event) => {
+    titlePreview.innerHTML = event.target.value;
+  });
+  inputJob.addEventListener('input', (event) => {
+    jobPreview.innerHTML = event.target.value;
+  });
 
 function closeOtherContainers(currentContainer) {
     if (currentContainer !== designContainer) {
@@ -34,6 +59,8 @@ function closeOtherContainers(currentContainer) {
     }
   }
 
+ 
+
   function handleArrowUp(container, arrow) {
     closeOtherContainers(container);
     toggleContainer(container);
@@ -41,9 +68,26 @@ function closeOtherContainers(currentContainer) {
     arrow.classList.toggle('rotate-down'); // Agregamos o quitamos la clase rotate-down
   }
 
+  function handleArrowUpShare () {
+    if (inputName.value == '' || inputJob.value == '' || inputEmail.value == '' || inputPhonealue == ''){
+      console.log('hey'); 
+      //requiredMessage.classList.remove('collapsed'); 
+      requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
+      //cambiar color casilla a rojo
+    } else if (inputEmailValue != inputEmailValue.contains('@')){
+      requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
+      //cambiar color casilla a rojo
+    } else if (inputPhoneValue != inputEmailValue.contains(Number)){
+      requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
+      //cambiar color casilla a rojo
+    } else {
+      handleArrowUp(); 
+    }
+  }
+
  
   arrowUpDesign.addEventListener('click', () => handleArrowUp(designContainer, arrowUpDesign));
   arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer, arrowUpForm));
-  arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer, arrowUpShare));
-  
+  arrowUpShare.addEventListener('click', (handleArrowUpShare)); 
+  //arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer, arrowUpShare));
   
--- src/js/autofill-card.js
+++ src/js/autofill-card.js
@@ -1,13 +1,13 @@
 
-const titlePreview = document.querySelector('.js_preview_title'); //variable del preview del nombre
-const inputName = document.querySelector('.js_name'); //variable nombre del formulario
-const jobPreview = document.querySelector('.js_preview_job');
-const inputJob = document.querySelector('.js_job_position');
+//const titlePreview = document.querySelector('.js_preview_title'); //variable del preview del nombre
+//const inputName = document.querySelector('.js_name'); (Variable declarada en el archivo de arrows)
+//const jobPreview = document.querySelector('.js_preview_job');
+//const inputJob = document.querySelector('.js_job_position'); (Variable declarada en el archivo de arrows)
 
 // Rellenar automáticamente cardpreview al escribir en el formulario
 inputName.addEventListener('input', (event) => {
     titlePreview.innerHTML = event.target.value;
-  });
-  inputJob.addEventListener('input', (event) => {
+});
+inputJob.addEventListener('input', (event) => {
     jobPreview.innerHTML = event.target.value;
-  });
+});
