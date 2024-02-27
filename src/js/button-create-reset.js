08cf0aff1c9002447cd84abbbe162e6b5a437dd7
Noelia <abascalcabreran@gmail.com>
2024-02-27 14:02:34 +0100

    changes

--- src/js/button-create-reset.js
+++ src/js/button-create-reset.js
@@ -1,7 +1,5 @@
-
 const buttonReset = document.querySelector('.js-reset-button');
 const buttonCreate = document.querySelector('.js_button_create_card');
-
 
 // BUTTON CREATE
 const handleClickButtonCreate = (event) => {
@@ -16,13 +14,13 @@ const handleClickButtonCreate = (event) => {
 
 buttonCreate.addEventListener('click', handleClickButtonCreate);
 
-
 // RESET BUTTON - resetCard
 const resetCard = () => {
   console.log('Resetting card');
   document.getElementById('card__title__id').innerHTML = 'Nombre Apellido';
   document.getElementById('card__job__id').innerHTML = 'Front-end developer';
-  document.getElementById('card__img__id').style.backgroundImage = 'url(https://i.pinimg.com/564x/da/f7/8c/daf78c186a9d4fa2bcfd1d84d9c956ca.jpg)';
+  document.getElementById('card__img__id').style.backgroundImage =
+    'url(https://i.pinimg.com/564x/da/f7/8c/daf78c186a9d4fa2bcfd1d84d9c956ca.jpg)';
   document.getElementById('social__email__id').href = '#';
   document.getElementById('social__mobile__id').href = 'tel:#';
   document.getElementById('social__linkedin__id').href = '#';
@@ -49,7 +47,19 @@ const resetPalette = () => {
 // resetProfileImage
 const resetPreviewImage = () => {
   profilePreview.style.backgroundImage = 'none';
-}
+};
+
+//resetColorTittlePalette
+const resetColorTittlePalette = () => {
+  if (tittle2 || tittle3 || tittle4 || tittle5 || tittle6.style.color !== '#54585a') {
+    title1.style.color = '#438792';
+    title2.style.color = '#54585a';
+    title3.style.color = '#54585a';
+    title4.style.color = '#54585a';
+    title5.style.color = '#54585a';
+    title6.style.color = '#54585a';
+  }
+};
 
 // resetMainImage
 // const resetMainImage = () => {
@@ -57,9 +67,10 @@ const resetPreviewImage = () => {
 // }
 const handleResetButtonClick = () => {
   resetCard();
-  resetForm()
+  resetForm();
   resetPalette();
   resetPreviewImage();
+  resetColorTittlePalette();
   // resetMainImage();
 };
 
@@ -68,38 +79,37 @@ buttonReset.addEventListener('click', handleResetButtonClick);
 // Social Media Links
 function updateContactLinks() {
   // Email
-  const emailInput = document.querySelector('.js_email'); 
+  const emailInput = document.querySelector('.js_email');
   const emailLink = document.getElementById('social__email__id');
-  emailInput.addEventListener('input', function() {
-    emailLink.href = 'mailto:' + emailInput.value; 
+  emailInput.addEventListener('input', function () {
+    emailLink.href = 'mailto:' + emailInput.value;
   });
 
   // Phone
-  const phoneInput = document.querySelector('.js_phone'); 
+  const phoneInput = document.querySelector('.js_phone');
   const phoneLink = document.getElementById('social__mobile__id');
-  phoneInput.addEventListener('input', function() {
-    phoneLink.href = 'tel:' + phoneInput.value; 
+  phoneInput.addEventListener('input', function () {
+    phoneLink.href = 'tel:' + phoneInput.value;
   });
 
- // LinkedIn
-const linkedInInput = document.querySelector('.js_linkedin'); 
-const linkedInLink = document.getElementById('social__linkedin__id');
+  // LinkedIn
+  const linkedInInput = document.querySelector('.js_linkedin');
+  const linkedInLink = document.getElementById('social__linkedin__id');
 
-linkedInInput.addEventListener('input', function() {
-  // URL base de LinkedIn
-  const baseUrl = 'https://www.linkedin.com/in/';
-  
-  // Concatena la URL base con el nombre de usuario ingresado
-  linkedInLink.href = baseUrl + linkedInInput.value; 
-});
+  linkedInInput.addEventListener('input', function () {
+    // URL base de LinkedIn
+    const baseUrl = 'https://www.linkedin.com/in/';
+
+    // Concatena la URL base con el nombre de usuario ingresado
+    linkedInLink.href = baseUrl + linkedInInput.value;
+  });
 
   // GitHub
-  const gitHubInput = document.querySelector('.js_github'); 
+  const gitHubInput = document.querySelector('.js_github');
   const gitHubLink = document.getElementById('social__github__id');
-  gitHubInput.addEventListener('input', function() {
+  gitHubInput.addEventListener('input', function () {
     const baseUrl = 'https://www.github.com/';
-  gitHubLink.href = baseUrl + gitHubInput.value; 
-  
+    gitHubLink.href = baseUrl + gitHubInput.value;
   });
 }
 
