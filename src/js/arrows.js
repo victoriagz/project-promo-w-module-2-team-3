4e03ad1d86f9d0b3a6aee823484eb873457e3bf7
Lejla <lejladzanko@gmail.com>
2024-02-27 18:39:12 +0100

    background no repeat fixed, arrows modified

--- src/js/arrows.js
+++ src/js/arrows.js
@@ -1,116 +1,46 @@
-const arrowUpDesign = document.querySelector('.js-arrow-up-design');
-const arrowUpForm = document.querySelector('.js-arrow-up-form');
-const arrowUpShare = document.querySelector('.js-arrow-up-share');
-const textDesign = document.querySelector('.js-text-design');
-const textForm = document.querySelector('.js-text-form');
-const textShare = document.querySelector('.js-text-share');
+const arrowUpDesign = document.querySelector(".js-arrow-up-design");
+const arrowUpForm = document.querySelector(".js-arrow-up-form");
+const arrowUpShare = document.querySelector(".js-arrow-up-share");
+const textDesign = document.querySelector(".js-text-design");
+const textForm = document.querySelector(".js-text-form");
+const textShare = document.querySelector(".js-text-share");
 
 //Containers
-const designContainer = document.querySelector('.js-color-container');
-const formContainer = document.querySelector('.js-form');
-const shareContainer = document.querySelector('.js-share');
-const dropDownSection = document.querySelector('.js_drop_down');
-const dropDownSectionCollapsed = dropDownSection.classList.add('collapsed');
+const designContainer = document.querySelector(".js-color-container");
+const formContainer = document.querySelector(".js-form");
+const shareContainer = document.querySelector(".js-share");
+const dropDownSection = document.querySelector(".js_drop_down");
+const dropDownSectionCollapsed = dropDownSection.classList.add("collapsed");
 
 //Form fields
-const inputName = document.querySelector('.js-name');
-const inputJob = document.querySelector('.js-job');
-const inputEmail = document.querySelector('.js-email');
-const inputPhone = document.querySelector('.js-phone');
-const requiredMessage = document.querySelector('.js-field-required-message');
-const titlePreview = document.querySelector('.js_preview_title'); //variable del preview del nombre
-const jobPreview = document.querySelector('.js_preview_job');
-
-//Form fields values 
-// const inputNameValue = inputName.value; 
-// const inputJobValue = inputJob.value; 
-// const inputEmailValue = inputEmail.value; 
-// const inputPhoneValue = inputPhone.value; 
-
-
-// Rellenar automáticamente cardpreview al escribir en el formulario
-// inputName.addEventListener('input', (event) => {
-//     titlePreview.innerHTML = event.target.value;
-//   });
-//   inputJob.addEventListener('input', (event) => {
-//     jobPreview.innerHTML = event.target.value;
-//   });
-
+const inputName = document.querySelector(".js-name");
+const inputJob = document.querySelector(".js-job");
+const inputEmail = document.querySelector(".js-email");
+const inputPhone = document.querySelector(".js-phone");
+const requiredMessage = document.querySelector(".js-field-required-message");
+const titlePreview = document.querySelector(".js_preview_title"); //variable del preview del nombre
+const jobPreview = document.querySelector(".js_preview_job");
 
 function closeOtherContainers(currentContainer) {
   if (currentContainer !== designContainer) {
-    designContainer.classList.add('collapsed');
+    designContainer.classList.add("collapsed");
   }
   if (currentContainer !== formContainer) {
-    formContainer.classList.add('collapsed');
+    formContainer.classList.add("collapsed");
   }
   if (currentContainer !== shareContainer) {
-    shareContainer.classList.add('collapsed');
+    shareContainer.classList.add("collapsed");
   }
 }
 
-function toggleContainer(container) {
-  container.classList.toggle('collapsed');
-}
-
-function handleArrowUp(container, arrow) {
+function handleCollapse(container) {
   closeOtherContainers(container);
-  toggleContainer(container);
-  arrow.classList.toggle('rotate-down'); // Agregamos o quitamos la clase rotate-down
+  container.classList.toggle("collapsed");
 }
 
-function handleText(container) {
-  closeOtherContainers(container);
-  toggleContainer(container);
-}
-
-arrowUpDesign.addEventListener('click', () => handleArrowUp(designContainer, arrowUpDesign));
-arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer, arrowUpForm));
-arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer, arrowUpShare));
-textDesign.addEventListener('click', () => handleText(designContainer, textDesign));
-textForm.addEventListener('click', () => handleText(formContainer, textForm));
-textShare.addEventListener('click', () => handleText(shareContainer, textShare));
-
-  
-  function toggleContainer(container) {
-    container.classList.toggle('collapsed');
-  }
-
-  function closeShareContainer (){
-    if (shareContainer.classList.contains('collapsed')){
-      if(dropDownSection !== dropDownSectionCollapsed){
-        dropDownSection.classList.add('collapsed');
-      }
-    }
-  }
-
-  function handleArrowUp(container, arrow) {
-    closeOtherContainers(container);
-    toggleContainer(container);
-    closeShareContainer(); 
-    arrow.classList.toggle('rotate-down'); // Agregamos o quitamos la clase rotate-down
-  }
-
-  // function handleArrowUpShare () {
-  //   if (inputName.value == '' || inputJob.value == '' || inputEmail.value == '' || inputPhonealue == ''){
-  //     console.log('hey'); 
-  //     //requiredMessage.classList.remove('collapsed'); 
-  //     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
-  //     //cambiar color casilla a rojo
-  //   } else if (inputEmailValue != inputEmailValue.contains('@')){
-  //     requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
-  //     //cambiar color casilla a rojo
-  //   } else if (inputPhoneValue != inputEmailValue.contains(Number)){
-  //     requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
-  //     //cambiar color casilla a rojo
-  //   } else {
-  //     handleArrowUp(); 
-  //   }
-  // }
-
- 
-  arrowUpDesign.addEventListener('click', () => handleArrowUp(designContainer, arrowUpDesign));
-  arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer, arrowUpForm));
-  //arrowUpShare.addEventListener('click', (handleArrowUpShare)); 
-  arrowUpShare.addEventListener('click', () => handleArrowUp(shareContainer, arrowUpShare));
-  
+textDesign.addEventListener("click", () => handleCollapse(designContainer, textDesign));
+textForm.addEventListener("click", () => handleCollapse(formContainer, textForm));
+textShare.addEventListener("click", () => handleCollapse(shareContainer, textShare));
+arrowUpDesign.addEventListener("click", () => handleCollapse(designContainer, arrowUpDesign));
+arrowUpForm.addEventListener("click", () => handleCollapse(formContainer, arrowUpForm));
+arrowUpShare.addEventListener("click", () => handleCollapse(shareContainer, arrowUpShare));
