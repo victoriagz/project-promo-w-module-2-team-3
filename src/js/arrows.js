7c9ecda87f32921565fd3d6072e243b858f0d508
BiancaMesa <bianca.koraa@gmail.com>
2024-02-28 19:56:56 +0100

    new way to check if the required fields are filled out

--- src/js/arrows.js
+++ src/js/arrows.js
@@ -2,7 +2,7 @@
 //Arrows
 const arrowUpDesign = document.querySelector('.js-arrow-up-design');
 const arrowUpForm = document.querySelector('.js-arrow-up-form');
-const arrowUpShare = document.querySelector('.js-arrow-up-share');
+const clickShare = document.querySelector('.js-arrow-up-share');
 
 //Containers
 const designContainer = document.querySelector('.js-color-container');
@@ -20,85 +20,8 @@ const requiredFields = document.querySelectorAll('.js-required'); //es un array
 const requiredMessage = document.querySelector('.js-field-required-message');
 //const requiredFields = form.querySelectorAll('[required]'); //variable que recoge todos los elementos con la clase required
 
-//Form fields values 
-//const inputRequiredValue = inputRequired.value; 
-// const inputNameValue = inputName.value; 
-// const inputJobValue = inputJob.value; 
-// const inputEmailValue = inputEmail.value; 
-// const inputPhoneValue = inputPhone.value; 
-
-//Función cambiar el color casilla a rojo
-// function changeFieldColor () {
-//   if (inputNameValue === '') {
-//     inputName.style.border = '1px solid #ad6868';
-//   }
-//   if (inputJobValue === '') {
-//     inputJob.style.border = '1px solid #ad6868';
-//   }
-//   if (inputEmailValue === '') {
-//     inputEmail.style.border = '1px solid #ad6868';
-//   }
-//   if (inputPhoneValue === '') {
-//   inputPhone.style.border = '1px solid #ad6868';
-//   }
-// }
-
-// function handleArrowUpShare () {
-//   requiredMessage.innerHTML = ''; 
-//   console.log('0'); 
-//   //const emptyField = inputNameValue == '' || inputJobValue == '' || inputEmailValue == '' || inputPhoneValue == '';
-//   //const filledOutForm = inputNameValue && inputJobValue && inputEmailValue && inputPhoneValue;
-  
-//   if (inputNameValue == '' || inputJobValue == '' || inputEmailValue == '' || inputPhoneValue == '') {
-//     console.log('1'); 
-//     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
-//     changeFieldColor(); 
-
-//   } else if (typeof inputPhoneValue !== 'number') { 
-//     console.log('3'); 
-//     requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
-//     inputPhone.style.border = '1px solid #ad6868';
-
-//   } else if (!inputEmail.value.includes('@')) {    
-//     console.log('2'); 
-//     requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
-//     inputEmail.style.border = '1px solid #ad6868';
-
-//   } else {
-//     handleArrowUp(shareContainer, arrowUpShare);  
-//   }
-// }
-
-  //Función manejadora del evento flecha de comparte 
-  // function handleArrowUpShare () {
-  //   console.log('0');
-  //   requiredMessage.innerHTML = ''; 
-    
-  //   //Bucle 
-  //   for (const field of requiredFields) {
-  //     console.log('1'); 
-
-  //     if (field.value === '') {
-  //       console.log('2');
-  //       field.value.border = '1px solid red';
-  //       requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
-
-  //     } else {
-  //       console.log('3');
-  //       field.value.border = ''; 
-  //       handleArrowUp(shareContainer, arrowUpShare);  
-  //     }
-  //   }
-  // }
-
-// Función para abrir el desplegable comparte
-function handleArrowUpShare () {
-
-  console.log('click arrow');
-
-  console.log('> requiredFields', requiredFields);
-
-   // que los campos required estén rellenos
+/* ANA 
+// que los campos required estén rellenos
     // Función que compruebe que todos los campos están rellenos --> devuelve true o false después de iterar por todos los field required
     // Si no están rellenos, muestro eso en el html y hago return
 
@@ -107,42 +30,105 @@ function handleArrowUpShare () {
 
      // handleArrowUp
 
+*/
+
+// Función que compruebe que todos los campos están rellenos --> devuelve true o false después de iterar por todos los field required
+let i = 0; 
+let result = ''; 
+
+function emptyField () { 
+
   for (const requiredField of requiredFields) {
-    
-    const emailField = requiredFields[2]; 
-    const emailFieldValue = emailField.value; 
-
-    const phoneField = requiredFields[3]; 
-    const phoneFieldValue = parseInt(phoneField.value); //para convertir el string a number porque todos los valores del input vienen como strings 
-
-    requiredField.style.border = '1px solid #a2deaf';
-
-    console.log('Required field', requiredField.name);
-    console.log('Required field value', requiredField.value); 
-    console.log('Required field typeof value', typeof requiredField.value); 
-
-
-
-    if (requiredField.value === '') {
-      requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
+    //if field empty
+    if(requiredField.value === '') {
+      i += 1; 
+      console.log(i); 
+      result = true;
       requiredField.style.border = '1px solid #ad6868';
-      console.log('1'); 
-      return;
-    } else if (!emailFieldValue.includes('@')) {  
-      conso
-      requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
-      emailField.style.border = '1px solid #ad6868';
-    } else if (isNaN(phoneFieldValue)) { 
-      // console.log('soy string'); 
-      requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
-      phoneField.style.border = '1px solid #ad6868';
+      console.log(result); 
+    //if field not empty 
     } else {
-      console.log('2');
-      handleArrowUp (shareContainer, arrowUpShare);
-      // console.log('hey');  
+      i = 0; 
+      console.log(i); 
+      console.log('campo relleno'); 
+      result = false;
+      requiredField.style.border = '';
+      requiredField.style.border = '1px solid #a2deaf';
+      //console.log(result); 
     }
   }
+
 }
+
+
+function handleCollapseShare () {
+  emptyField(); 
+
+  const nameField = requiredFields[0];
+  const nameFieldValue = nameField.value; 
+  const jobField = requiredFields[1];
+  const jobFieldValue = jobField.value; 
+  const emailField = requiredFields[2]; 
+  const emailFieldValue = emailField.value; 
+  const phoneField = requiredFields[3]; 
+  const phoneFieldValue = parseInt(phoneField.value);
+
+
+  if (nameFieldValue && jobFieldValue && emailField && phoneFieldValue) {
+    console.log('todos los campos rellenos'); 
+    requiredMessage.innerHTML = 'Todos los campos están rellenos, buen trabajo!'; 
+    handleCollapse (shareContainer, clickShare);
+  } else if (result === true) {
+    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*'; 
+    //requiredField.style.border = '1px solid #ad6868';
+  } else if (!emailFieldValue.includes('@')) {  
+      requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
+      emailField.style.border = '1px solid #ad6868';
+  } else if (isNaN(phoneFieldValue)) { 
+      requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
+      phoneField.style.border = '1px solid #ad6868';
+  }  
+}
+
+// Función para abrir el desplegable comparte
+// function handleArrowUpShare () {
+
+//   for (const requiredField of requiredFields) {
+    
+//     const emailField = requiredFields[2]; 
+//     const emailFieldValue = emailField.value; 
+
+//     const phoneField = requiredFields[3]; 
+//     const phoneFieldValue = parseInt(phoneField.value); //para convertir el string a number porque todos los valores del input vienen como strings 
+
+//     requiredField.style.border = '1px solid #a2deaf';
+
+//     console.log('Required field', requiredField.name);
+//     console.log('Required field value', requiredField.value); 
+//     console.log('Required field typeof value', typeof requiredField.value); 
+
+
+
+//     if (requiredField.value === '') {
+//       requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan'; 
+//       requiredField.style.border = '1px solid #ad6868';
+//       console.log('1'); 
+//       return;
+//     } else if (!emailFieldValue.includes('@')) {  
+//       conso
+//       requiredMessage.innerHTML = 'Por favor, introduce un email válido'; 
+//       emailField.style.border = '1px solid #ad6868';
+//     } else if (isNaN(phoneFieldValue)) { 
+//       // console.log('soy string'); 
+//       requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido'; 
+//       phoneField.style.border = '1px solid #ad6868';
+//     } else {
+//       console.log('2');
+//       handleArrowUp (shareContainer, arrowUpShare);
+//       // console.log('hey');  
+//     }
+//   }
+// }
 
   
   function closeOtherContainers(currentContainer) {
@@ -171,7 +157,7 @@ function handleArrowUpShare () {
   }
  
 
-  function handleArrowUp(container, arrow) {
+  function handleCollapse(container, arrow) {
     // console.log('0'); 
     closeOtherContainers(container);
     // console.log('1'); 
@@ -179,13 +165,14 @@ function handleArrowUpShare () {
     // console.log('2');  
     closeShareContainer(); 
     // console.log('3'); 
-    arrow.classList.toggle('rotate-down'); // Agregamos o quitamos la clase rotate-down
+    arrow.classList.toggle('rotate-down'); 
     // console.log('4'); 
   }
 
  
-  arrowUpDesign.addEventListener('click', () => handleArrowUp(designContainer, arrowUpDesign));
-  arrowUpForm.addEventListener('click', () => handleArrowUp(formContainer, arrowUpForm));
-  arrowUpShare.addEventListener('click', () => handleArrowUpShare());
+  arrowUpDesign.addEventListener('click', () => handleCollapse(designContainer, arrowUpDesign));
+  arrowUpForm.addEventListener('click', () => handleCollapse(formContainer, arrowUpForm));
+  clickShare.addEventListener('click', () => handleCollapseShare());
 
   
+
