2ea345a2345b551ccb2baf1425cf5acff91f0790
BiancaMesa <bianca.koraa@gmail.com>
2024-06-06 12:35:36 +0200

    collapsables partial commented

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -1,154 +1,157 @@
-//Arrows & texts
-const arrowUpDesign = document.querySelector('js-arrow-up-design');
-const arrowUpForm = document.querySelector('.js-arrow-up-form');
-const arrowUpShare = document.querySelector('.js-arrow-up-share');
-const textDesign = document.querySelector('.js-text-design');
-const textForm = document.querySelector('.js-text-form');
-const textShare = document.querySelector('.js-text-share');
 
-//Containers
-const designContainer = document.querySelector('.js-color-container');
-const formContainer = document.querySelector('.js-form');
-const shareContainer = document.querySelector('.js-share');
-const dropDownSection = document.querySelector('.js_drop_down');
 
-//Create button
-const buttonCreate = document.querySelector('.js_button_create_card');
 
-//Form fields
-const inputPalette = document.querySelector('.color__container--group');
-const inputName = document.querySelector('.js-name');
-const inputJob = document.querySelector('.js-job');
-const inputEmail = document.querySelector('.js-email-2');
-const inputPhone = document.querySelector('.js-phone');
-const inputLinkedin = document.querySelector('.js-linkedin-2');
-const inputGithub = document.querySelector('.js-github-2');
-const inputImage = document.querySelector('.js-image');
-const requiredFields = document.querySelectorAll('.js-required');
-const requiredMessage = document.querySelector('.js-field-required-message');
+// //Arrows & texts
+// const arrowUpDesign = document.querySelector('js-arrow-up-design');
+// const arrowUpForm = document.querySelector('.js-arrow-up-form');
+// const arrowUpShare = document.querySelector('.js-arrow-up-share');
+// const textDesign = document.querySelector('.js-text-design');
+// const textForm = document.querySelector('.js-text-form');
+// const textShare = document.querySelector('.js-text-share');
 
-function handleCollapseShare() {
-  console.log('successful deploy');
+// //Containers
+// const designContainer = document.querySelector('.js-color-container');
+// const formContainer = document.querySelector('.js-form');
+// const shareContainer = document.querySelector('.js-share');
+// const dropDownSection = document.querySelector('.js_drop_down');
 
-  const nameField = requiredFields[0];
-  const nameFieldValue = nameField.value;
+// //Create button
+// const buttonCreate = document.querySelector('.js_button_create_card');
 
-  const jobField = requiredFields[1];
-  const jobFieldValue = jobField.value;
+// //Form fields
+// const inputPalette = document.querySelector('.color__container--group');
+// const inputName = document.querySelector('.js-name');
+// const inputJob = document.querySelector('.js-job');
+// const inputEmail = document.querySelector('.js-email-2');
+// const inputPhone = document.querySelector('.js-phone');
+// const inputLinkedin = document.querySelector('.js-linkedin-2');
+// const inputGithub = document.querySelector('.js-github-2');
+// const inputImage = document.querySelector('.js-image');
+// const requiredFields = document.querySelectorAll('.js-required');
+// const requiredMessage = document.querySelector('.js-field-required-message');
 
-  const emailField = requiredFields[2];
-  const emailFieldValue = emailField.value;
+// function handleCollapseShare() {
+//   console.log('successful deploy');
 
-  const phoneField = requiredFields[3];
-  const phoneFieldNumber = parseInt(phoneField.value);
+//   const nameField = requiredFields[0];
+//   const nameFieldValue = nameField.value;
 
-  const linkedinField = requiredFields[4];
-  const linkedinFieldValue = linkedinField.value;
+//   const jobField = requiredFields[1];
+//   const jobFieldValue = jobField.value;
 
-  const githubField = requiredFields[5];
-  const githubFieldValue = githubField.value;
+//   const emailField = requiredFields[2];
+//   const emailFieldValue = emailField.value;
 
-  if (nameFieldValue === '') {
-    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
-    nameField.style.border = '1px solid #ad6868';
-    nameField.focus();
-    return;
-  }
-  nameField.style.border = '1px solid #a2deaf';
+//   const phoneField = requiredFields[3];
+//   const phoneFieldNumber = parseInt(phoneField.value);
 
-  if (jobFieldValue === '') {
-    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
-    jobField.style.border = '1px solid #ad6868';
-    jobField.focus();
-    return;
-  }
-  jobField.style.border = '1px solid #a2deaf';
+//   const linkedinField = requiredFields[4];
+//   const linkedinFieldValue = linkedinField.value;
 
-  if (emailFieldValue === '') {
-    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
-    emailField.style.border = '1px solid #ad6868';
-    emailField.focus();
-    return;
-  }
-  emailField.style.border = '1px solid #a2deaf';
+//   const githubField = requiredFields[5];
+//   const githubFieldValue = githubField.value;
 
-  if (!emailFieldValue.includes('@')) {
-    requiredMessage.innerHTML = 'Por favor, introduce un email válido*';
-    emailField.style.border = '1px solid #ad6868';
-    emailField.focus();
-    return;
-  }
-  emailField.style.border = '1px solid #a2deaf';
+//   if (nameFieldValue === '') {
+//     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
+//     nameField.style.border = '1px solid #ad6868';
+//     nameField.focus();
+//     return;
+//   }
+//   nameField.style.border = '1px solid #a2deaf';
 
-  if (phoneFieldNumber === '') {
-    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
-    phoneField.style.border = '1px solid #ad6868';
-    phoneField.focus();
-    return;
-  }
-  phoneField.style.border = '1px solid #a2deaf';
+//   if (jobFieldValue === '') {
+//     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
+//     jobField.style.border = '1px solid #ad6868';
+//     jobField.focus();
+//     return;
+//   }
+//   jobField.style.border = '1px solid #a2deaf';
 
-  if (isNaN(phoneFieldNumber)) {
-    requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido*';
-    phoneField.style.border = '1px solid #ad6868';
-    phoneField.focus();
-    return;
-  }
-  phoneField.style.border = '1px solid #a2deaf';
+//   if (emailFieldValue === '') {
+//     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
+//     emailField.style.border = '1px solid #ad6868';
+//     emailField.focus();
+//     return;
+//   }
+//   emailField.style.border = '1px solid #a2deaf';
 
-  if (linkedinFieldValue === '') {
-    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
-    linkedinField.style.border = '1px solid #ad6868';
-    linkedinField.focus();
-    return;
-  }
-  linkedinField.style.border = '1px solid #a2deaf';
+//   if (!emailFieldValue.includes('@')) {
+//     requiredMessage.innerHTML = 'Por favor, introduce un email válido*';
+//     emailField.style.border = '1px solid #ad6868';
+//     emailField.focus();
+//     return;
+//   }
+//   emailField.style.border = '1px solid #a2deaf';
 
-  if (githubFieldValue === '') {
-    requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
-    githubField.style.border = '1px solid #ad6868';
-    githubField.focus();
-    return;
-  }
-  githubField.style.border = '1px solid #a2deaf';
+//   if (phoneFieldNumber === '') {
+//     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
+//     phoneField.style.border = '1px solid #ad6868';
+//     phoneField.focus();
+//     return;
+//   }
+//   phoneField.style.border = '1px solid #a2deaf';
 
-  console.log('todos los campos rellenos');
-  requiredMessage.innerHTML = '';
-  handleCollapse(shareContainer, arrowUpShare);
-}
+//   if (isNaN(phoneFieldNumber)) {
+//     requiredMessage.innerHTML = 'Por favor, introduce un teléfono válido*';
+//     phoneField.style.border = '1px solid #ad6868';
+//     phoneField.focus();
+//     return;
+//   }
+//   phoneField.style.border = '1px solid #a2deaf';
 
-function closeOtherContainers(currentContainer) {
-  if (currentContainer !== designContainer) {
-    designContainer.classList.add('collapsed');
-  }
-  if (currentContainer !== formContainer) {
-    formContainer.classList.add('collapsed');
-  }
-  if (currentContainer !== shareContainer) {
-    shareContainer.classList.add('collapsed');
-  }
-}
+//   if (linkedinFieldValue === '') {
+//     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
+//     linkedinField.style.border = '1px solid #ad6868';
+//     linkedinField.focus();
+//     return;
+//   }
+//   linkedinField.style.border = '1px solid #a2deaf';
 
-function handleCollapse(container) {
-  closeOtherContainers(container);
-  container.classList.toggle('collapsed');
-}
+//   if (githubFieldValue === '') {
+//     requiredMessage.innerHTML = 'Por favor, rellena los campos que faltan*';
+//     githubField.style.border = '1px solid #ad6868';
+//     githubField.focus();
+//     return;
+//   }
+//   githubField.style.border = '1px solid #a2deaf';
 
-// Collapsable create button
-const handleClickButtonCreate = (event) => {
-  const dropDown = document.querySelector('.js_drop_down');
-  event.preventDefault();
-  if (dropDown.classList.contains('collapsed')) {
-    dropDown.classList.remove('collapsed');
-    buttonCreate.style.backgroundColor = '#d5d5d5';
-    buttonCreate.style.pointerEvents = 'none';
-  }
-};
+//   console.log('todos los campos rellenos');
+//   requiredMessage.innerHTML = '';
+//   handleCollapse(shareContainer, arrowUpShare);
+// }
 
-buttonCreate.addEventListener('click', handleClickButtonCreate);
-textDesign.addEventListener('click', () => handleCollapse(designContainer, textDesign));
-textForm.addEventListener('click', () => handleCollapse(formContainer, textForm));
-textShare.addEventListener('click', () => handleCollapseShare());
-arrowUpDesign.addEventListener('click', () => handleCollapse(designContainer, arrowUpDesign));
-arrowUpForm.addEventListener('click', () => handleCollapse(formContainer, arrowUpForm));
-arrowUpShare.addEventListener('click', () => handleCollapseShare());
+// function closeOtherContainers(currentContainer) {
+//   if (currentContainer !== designContainer) {
+//     designContainer.classList.add('collapsed');
+//   }
+//   if (currentContainer !== formContainer) {
+//     formContainer.classList.add('collapsed');
+//   }
+//   if (currentContainer !== shareContainer) {
+//     shareContainer.classList.add('collapsed');
+//   }
+// }
+
+// function handleCollapse(container) {
+//   closeOtherContainers(container);
+//   container.classList.toggle('collapsed');
+// }
+
+// // Collapsable create button
+// const handleClickButtonCreate = (event) => {
+//   const dropDown = document.querySelector('.js_drop_down');
+//   event.preventDefault();
+//   if (dropDown.classList.contains('collapsed')) {
+//     dropDown.classList.remove('collapsed');
+//     buttonCreate.style.backgroundColor = '#d5d5d5';
+//     buttonCreate.style.pointerEvents = 'none';
+//   }
+// };
+
+// buttonCreate.addEventListener('click', handleClickButtonCreate);
+// textDesign.addEventListener('click', () => handleCollapse(designContainer, textDesign));
+// textForm.addEventListener('click', () => handleCollapse(formContainer, textForm));
+// textShare.addEventListener('click', () => handleCollapseShare());
+// arrowUpDesign.addEventListener('click', () => handleCollapse(designContainer, arrowUpDesign));
+// arrowUpForm.addEventListener('click', () => handleCollapse(formContainer, arrowUpForm));
+// arrowUpShare.addEventListener('click', () => handleCollapseShare());
